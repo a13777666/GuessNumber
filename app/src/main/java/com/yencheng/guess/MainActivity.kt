8453d9a5289ee3e9038413b78c8fef8ffcf865f7
Class <a13777666@gmail.com>
2022-03-04 10:40:57 +0800

    GuessNumber second

--- app/src/main/java/com/yencheng/guess/MainActivity.kt
+++ app/src/main/java/com/yencheng/guess/MainActivity.kt
@@ -16,9 +16,14 @@ class MainActivity : AppCompatActivity() {
     val secret = (1..100).random()
     var max=100
     var min=1
+    var count = 10
 
 
     fun guess(view: View){
+
+        binding.tvResult.visibility = View.VISIBLE
+        binding.tvChances.visibility = View.VISIBLE
+        binding.tvChances.text = "You have $count chances"
 
         var number = binding.edNumber.text.toString().toInt()
 
@@ -29,13 +34,17 @@ class MainActivity : AppCompatActivity() {
 
         if (number > secret) {
             max = number
-            binding.tvResult.text = "Please enter a number($min to $max):"
         } else if (number < secret) {
             min = number
-            binding.tvResult.text = "Please enter a number($min to $max):"
         } else {
             binding.tvResult.text = "You got it!! The number is $secret"
         }
+        binding.tvResult.text = "Please enter a number($min to $max):"
+        if (count <= 0) {
+            binding.tvResult.text = "Time's up !! You suck!!"
+            return
+        }
+        count--
 
     }
 }
